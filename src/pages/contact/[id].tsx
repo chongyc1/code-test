768a225629c3a22ce84474c4b9a58582f3263f6a
chongyc1 <yuechiac@hotmail.com>
2023-03-21 14:56:24 +0800

    ✨ feat: added ssr load contact detail

--- src/pages/contact/[id].tsx
+++ src/pages/contact/[id].tsx
@@ -2,15 +2,38 @@ import { useRouter } from 'next/router';
 import styles from '@/styles/contact_page.module.scss';
 import { Avatar, Card, Col, message, Row, Spin, Table } from 'antd';
 import { useEffect } from 'react';
-import useContactDetail, { EpisodeType } from '@/hooks/useContactDetail';
+import { EpisodeType } from '@/hooks/useContactDetail';
 import ContactInfo from '@/components/ContactInfo';
 import { ColumnsType } from 'antd/es/table';
 import Head from 'next/head';
+import { getContactDetail } from '@/apis/contact';
+import { getEpisodeList } from '@/apis/episode';
 
-const ContactDetail = () => {
+type ContactDetailType = {
+  name: string,
+  avatar: string,
+  status: string,
+  gender: string,
+  species: string,
+  location: {
+    name: string,
+  },
+  origin: {
+    name: string,
+  },
+  episodes: Array<EpisodeType>,
+}
+
+interface ContactDetailProps {
+  title: string;
+  detail: ContactDetailType;
+  loading: boolean;
+}
+
+const ContactDetail = ({ title = '', detail, loading }: ContactDetailProps) => {
   const router = useRouter();
 
-  const [loading, detail] = useContactDetail(router.query.id as string);
+  // const [loading, detail] = useContactDetail(router.query.id as string);
 
   const columns: ColumnsType<EpisodeType> = [
     {
@@ -28,16 +51,16 @@ const ContactDetail = () => {
   ];
 
   useEffect(() => {
-    if (detail === undefined) {
+    if (loading === true) {
       router.push("/contact");
       message.error("Invalid Contact");
     }
-  }, [detail, router])
+  }, [loading, router])
 
   return <>
     <Head>
-      <title>{detail?.name} - SleekFlow</title>
-      <meta name="description" content={`View information about ${detail?.name}`} />
+      <title>{title} - SleekFlow</title>
+      <meta name="description" content={`View information about ${title}`} />
     </Head>
     <Spin spinning={loading}>
       <div className={styles.header}>
@@ -62,9 +85,9 @@ const ContactDetail = () => {
           </Card>
           <br /><hr /><br />
           <h3>Episodes:</h3>
-          <small className='block'>Total In {detail?.episodes.length}
+          {/* <small className='block'>Total In {detail?.episodes.length}
             &nbsp;Episode{detail?.episodes.length === 1 ? '' : 's'}
-          </small><br />
+          </small><br /> */}
           <Table
             columns={columns}
             dataSource={detail?.episodes}
@@ -75,5 +98,49 @@ const ContactDetail = () => {
     </Spin>
   </>;
 }
+
+export const getServerSideProps = async (context: any) => {
+  const { id } = context.query;
+
+  var loading = true;
+  var detail: Partial<ContactDetailType> = {};
+  // fetch data from an API using the ID
+  const ret = await getContactDetail(id as string);
+  if (ret.status === 200) {
+    var allEpList = Array.isArray(ret.data.episode) ? ret.data.episode : [];
+    const allEps = allEpList.map((ep: string) => ep.replace('https://rickandmortyapi.com/api/episode/', '')).join(',');
+
+    const epData = await getEpisodeList(allEps);
+    var epList = [];
+    if (epData.status === 200) {
+      //some character only got 1 ep , the api not return [{...}] but return {...}
+      epList = Array.isArray(epData.data) ? epData.data : epData.data.id !== undefined ? [epData.data] : [];
+    }
+    detail = {
+      name: ret.data.name,
+      avatar: ret.data.image,
+      status: ret.data.status,
+      gender: ret.data.gender,
+      species: ret.data.species,
+      location: {
+        name: ret.data.location?.name,
+      },
+      origin: {
+        name: ret.data.origin?.name,
+      },
+      episodes: epList,
+    };
+    loading = false;
+  }
+
+  // pass the data to the component
+  return {
+    props: {
+      title: ret.data.name,
+      loading: loading,
+      detail: detail,
+    },
+  };
+};
 
 export default ContactDetail;
